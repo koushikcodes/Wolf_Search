78cdd54d355564c731a4e5d35345d9cacadd95aa
AnonWiz <koushik5495sahoo@gmail.com>
2018-02-03 06:14:06 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup,Executable
-setup(name='Wolfram Query',
-       version='1.0',
-       description='Wolfram Query __*made by AnonWiz',
-       executables= [Executable("wolf.py")])
-         
